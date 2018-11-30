d3e60f2b790baabd7a2930335b28803f9aa97bf8
ChakoChen <hengson_chen@hotmail.com>
2018-11-30 17:14:23 -0330

    Add files via upload

--- oil_trajectory_slider.py
+++ oil_trajectory_slider.py
@@ -8,35 +8,56 @@ from bokeh.palettes import Spectral6
 from datetime import datetime, date, time, timedelta
 import pandas as pd
 
-StartDateTime = '2018-11-30 00:00:00'
+# Specify data file name
+filename = '../data/SWRX_OIL20181130.csv'
+
+def prepare_data(filename):
+    """
+    Prepare the data by converting UTC time to local time, and add new Series for plot
+    """
+    # Import data
+    data = pd.read_csv(filename, delimiter=",")
+
+    # Convert UTC time to Newfoundland time
+    data['LocalDateTime'] = pd.to_datetime(data['DateTime']).dt.tz_localize('UTC').dt.tz_convert('America/St_Johns').dt.tz_localize(None).dt.strftime('%Y-%m-%d %H:%M:%S')
+
+    # Define a scaled radius for circle plots
+    data['ScaledRadius'] = data['Radius']/15000.  # cirle size
+
+    # Set index to DateTime
+    data = data.set_index(pd.DatetimeIndex(data['LocalDateTime']))
+     
+    return data 
 
 def myapp():
     """
     Build the oil trajectory visulization app
     """
-
     ####################################
     # (0) Construct a ColumnDataSource #
     ####################################
 
-    # Import data
-    data = pd.read_csv('../data/SWRX_OIL20181130.csv', delimiter=",")
-
-    # Convert UTC time to Newfoundland time
-    data['LocalDateTime'] = pd.to_datetime(data['DateTime']).dt.tz_localize('UTC').dt.tz_convert('America/St_Johns').dt.tz_localize(None).dt.strftime('%Y-%m-%d %H:%M')
+    # Read data
+    data = prepare_data(filename)
    
-    # Set index to DateTime
-    data = data.set_index(pd.DatetimeIndex(data['LocalDateTime']))
+    # Get the beginning time of oil spill
+    StartDateTime = str(data['LocalDateTime'][0]) 
 
-    # Define a scaled radius for circle plots
-    data['ScaledRadius'] = data['Radius']/15000.  # cirle size
-
-    # Make a static ColumnDataSource for background plot
-    StaticSource = ColumnDataSource(data={
+    # Make a ColumnDataSource for background plot
+    BackgroundSource = ColumnDataSource(data={
         'x'         : data.loc[:,'Longitude'],
         'y'         : data.loc[:,'Latitude'],
         'radius'    : data.loc[:,'ScaledRadius'],
-        'thickness' : data.loc[:,'Thickness']
+        'thickness' : data.loc[:,'Thickness'],
+        'Radius'    : data.loc[:,'Radius']
+    })
+
+    # Make a ColumnDataSource for line plot
+    LineSource = ColumnDataSource(data={
+        'x' : data.loc[:,'Longitude'].resample('2H').mean(),
+        'y' : data.loc[:,'Latitude'].resample('2H').mean(),
+        'thickness' : data.loc[:,'Thickness'].resample('2H').mean(),
+        'Radius' : data.loc[:,'Radius'].resample('2H').mean()
     })
 
     # Make an initial ColumnDataSource: source
@@ -44,7 +65,8 @@ def myapp():
         'x'         : data.loc[StartDateTime,'Longitude'],
         'y'         : data.loc[StartDateTime,'Latitude'],
         'radius'    : data.loc[StartDateTime,'ScaledRadius'],
-        'thickness' : data.loc[StartDateTime,'Thickness']
+        'thickness' : data.loc[StartDateTime,'Thickness'],
+        'Radius'    : data.loc[StartDateTime,'Radius']
     })
 
     # Save the minimum and maximum values of the longitude column: xmin, xmax
@@ -77,7 +99,8 @@ def myapp():
             'x'         : data.loc[str(NewDateTime),'Longitude'],
             'y'         : data.loc[str(NewDateTime),'Latitude'],
             'radius'    : data.loc[str(NewDateTime),'ScaledRadius'],
-            'thickness' : data.loc[str(NewDateTime),'Thickness']
+            'thickness' : data.loc[str(NewDateTime),'Thickness'],
+            'Radius'    : data.loc[str(NewDateTime),'Radius']
         }
         
         # Assign new_data to source.data
@@ -105,16 +128,19 @@ def myapp():
     # (1.2) Add a HoverTool #
     #-----------------------#
     # Create a HoverTool: hover
-    hover = HoverTool(tooltips=[('Longitude','@x'),('Latitude','@y'),('Radius','@radius{0.0000}'),('Thickness','@thickness{0.0000}')])
+    hover = HoverTool(tooltips=[('Longitude','@x'),('Latitude','@y'),('Radius','@Radius{0.0000}'),('Thickness','@thickness{0.0000}')])
 
     #------------------------------------------------#
     # (1.3) Add the initial circle glypf to the plot #
     #------------------------------------------------#
     # Plot a circle glyph as a background
-    plot.scatter(source=StaticSource, x='x', y='y', radius='radius', marker='circle', alpha=0.5, fill_color='grey', line_color='white')
+    plot.scatter(source=BackgroundSource, x='x', y='y', radius='radius', marker='circle', alpha=0.5, fill_color='grey', line_color='white')
+
+    # Add an average path by resamping the DataFrame
+    plot.line(source=LineSource, x='x', y='y', color='grey', line_width=1.5, alpha=0.8)
 
     # Add circle glyphs to the plot
-    plot.scatter(source=source, x='x', y='y', radius='radius', marker='circle', alpha=1, fill_color='black', line_color='white', hover_fill_color='firebrick', hover_alpha=0.5, hover_line_color='white')
+    plot.scatter(source=source, x='x', y='y', radius='radius', marker='circle', alpha=1, fill_color='black', line_color='grey', hover_fill_color='firebrick', hover_alpha=0.5, hover_line_color='white')
 
     # Add the HoverTool to the plot
     plot.add_tools(hover)
